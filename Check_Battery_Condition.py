2dd4427d738322e8da029ad5d906f2cb9be1065d
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 15:10:52 +0530

    Update Check_Battery_Condition.py

--- Check_Battery_Condition.py
+++ Check_Battery_Condition.py
@@ -1,13 +1,12 @@
 from Check_Temperature import *
 from Check_State_Of_Charge import *
-from Check_Charge_Rate import *
+# from Check_Charge_Rate import *
 
-def battery_is_ok(temperature, soc, charge_rate):
+def battery_is_ok(temperature, soc):
   battery_condition = False
   battery_temperature = verify_temperature_threshold(temperature)
   battery_soc = verify_state_of_charge_threshold(soc)
-  battery_charge_rate = verify_charge_rate_threshold(charge_rate)
-  if(battery_temperature and battery_soc and battery_charge_rate):
+  if(battery_temperature and battery_soc):
         battery_condition = True
         return battery_condition
   return battery_condition

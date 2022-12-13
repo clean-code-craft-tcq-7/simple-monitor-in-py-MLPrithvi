62373c6d44920e879c4d4d0f01e72abdeee3b411
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 14:53:04 +0530

    Update Print_Battery_Condition.py

--- Print_Battery_Condition.py
+++ Print_Battery_Condition.py
@@ -1,15 +1,9 @@
 from Check_Battery_Condition import *
 
 def print_battery_properties(temperature, soc, charge_rate):
-    if temperature == True:
-        print(f'Temperature of the Battery is {temperature} & is within the limit')
-    else:
+    if not (temperature):
         print(f'Temperature of the Battery is {temperature} & has exceeded the limit')
-    if soc == True:
-        print(f'State of Charge of the Battery is {soc} & is within the limit')
-    else:
+    if not (soc):
         print(f'State of Charge of the Battery is {soc} & has exceeded the limit')
-    if charge_rate == True:
-        print(f'Charge Rate of the Battery is {charge_rate} & the battery life is in good condition')
-    else:
+    if not (charge_rate):
         print(f'Charge Rate of the Battery is {charge_rate} & can affect the battery life')

9da6136af6c0bb5a2546914e2fcc4ef83384a832
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 13:55:00 +0530

    Update Check_Temperature.py

--- Check_Temperature.py
+++ Check_Temperature.py
@@ -1,6 +1,6 @@
 from Threshold_Limit_Constant import *
 
-def verify_temperature_threshold(temp)
+def verify_temperature_threshold(temp):
   if temp < MIN_TEMPERATURE or temp > MAX_TEMPERATURE:
     return False
   return True

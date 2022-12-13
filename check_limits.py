534b15f5d46a944698633f03f4f4fff63eaa91e0
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 15:18:10 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
   assert(battery_is_ok(1, 19) is False)
   assert(battery_is_ok(28, 81) is False)
   assert(battery_is_ok(25, 70) is True)
+  assert(battery_is_ok(-20, 9) is False)
   assert(battery_is_ok(100, 100) is False)
   print("All Test Scenarios Covered")

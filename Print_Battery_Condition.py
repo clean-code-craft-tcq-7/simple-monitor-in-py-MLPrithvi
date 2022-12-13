0462d8bda44e122c0f4c5cf243ebb457311f768f
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 15:05:19 +0530

    Delete Print_Battery_Condition.py

--- Print_Battery_Condition.py
+++
@@ -1,9 +0,0 @@
-from Check_Battery_Condition import *
-
-def print_battery_properties(temperature, soc, charge_rate):
-    if not (temperature):
-        print(f'Temperature of the Battery is {temperature} & has exceeded the limit')
-    if not (soc):
-        print(f'State of Charge of the Battery is {soc} & has exceeded the limit')
-    if not (charge_rate):
-        print(f'Charge Rate of the Battery is {charge_rate} & can affect the battery life')

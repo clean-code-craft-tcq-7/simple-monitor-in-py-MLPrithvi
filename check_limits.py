a2082faf185b970fa7257cbd63a25056cded19cf
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2022-12-13 14:45:28 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,12 +1,13 @@
-from Check_Temperature import *
-from Check_State_Of_Charge import *
-from Check_Charge_Rate import *
-
-def battery_is_ok(temperature, soc, charge_rate):
-  battery_condition = False
-  battery_temperature = verify
-
+from Check_Battery_Condition import *
 
 if __name__ == '__main__':
+  assert(battery_is_ok(-1, 50, 0.7) is False)
+  assert(battery_is_ok(46, 60, 0.25) is False)
+  assert(battery_is_ok(1, 19, 0.4) is False)
+  assert(battery_is_ok(28, 81, 0.3) is False)
+  assert(battery_is_ok(10, 30, 0.81) is False)
   assert(battery_is_ok(25, 70, 0.7) is True)
   assert(battery_is_ok(50, 85, 0) is False)
+  assert(battery_is_ok(40, 90, .9) is False)
+  assert(battery_is_ok(70, 45, 1) is False)
+  print("All Test Scenarios Covered")
